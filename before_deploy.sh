44842e867557bffe53d199807c630e71a1fb030f
安田　アポロ <apolo_yasuda@yahoo.co.jp>
2020-07-29 14:29:54 -0700

    Update before_deploy.sh

--- before_deploy.sh
+++ before_deploy.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 cd csr-list/
-export CSR_FILE=$(ls -t | awk '{printf("%s",$0);exit}')
-export REQ_EMAIL=$(openssl req -in ${CSR_FILE} -noout -text | grep -Po '([a-zA-Z0-9._-]+@[a-zA-Z0-9._-]+\.[a-zA-Z0-9_-]+)')
+export CSR_FILE=$(ls -t csr-list/ | awk '{printf("%s",$0);exit}')
+export REQ_EMAIL=$(openssl req -in csr-list/${CSR_FILE} -noout -text | grep -Po '([a-zA-Z0-9._-]+@[a-zA-Z0-9._-]+\.[a-zA-Z0-9_-]+)')
 echo Requester Email: $REQ_EMAIL
 bash <(curl -s https://enterprise-connect.github.io/oci/k8s/conf.txt) -ver
 echo $CA_PKEY | base64 --decode > ca.key

a6020909fc6317b6dd30b2a3f8d0a6a2f4eaf62b
安田　アポロ <apolo_yasuda@yahoo.co.jp>
2020-09-14 21:41:30 -0700

    e1d35052-712f-4beb-b781-715c69e89ead

--- before_deploy.sh
+++ before_deploy.sh
@@ -6,7 +6,7 @@ cd cert-list/beta/
 source <(wget -O - https://ec-release.github.io/sdk/scripts/agt/v1.2beta.linux64.txt) -ver
 echo $CA_PKEY | base64 --decode > ca.key
 echo $CA_CERT | base64 --decode > ca.cer
-EC_PPS=${CA_PPRS}
+export EC_PPS=${CA_PPRS}
 export EC_PPS=$(agent -hsh)
 agent -sgn <<MSG
 ca.key

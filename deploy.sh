7ce4150ab8e92e50f994f7dd6d351f379b52a3a5
A. Yasuda <70172309+ayasuda-ge@users.noreply.github.com>
2020-12-17 08:50:04 -0800

    Update deploy.sh

--- deploy.sh
+++ deploy.sh
@@ -2,5 +2,6 @@
 git config user.email "EC.Bot@ge.com"
 git config user.name "EC Bot"
 git add cert-list/.
+git checkout .
 git commit -m 'licensed to '$CSR_ID' [skip ci]'
 git push https://${GITHUB_TOKEN}@github.com/EC-Release/certifactory.git HEAD:release

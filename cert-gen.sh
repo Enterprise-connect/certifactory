5b1043d57ea7d21bb9865ae04cb9c984f3577d9d
A. Yasuda <70172309+ayasuda-ge@users.noreply.github.com>
2021-03-18 13:50:38 -0700

    [skip ci]

--- cert-gen.sh
+++ cert-gen.sh
@@ -21,7 +21,8 @@ source <(wget -O - https://raw.githubusercontent.com/EC-Release/sdk/disty/script
   agent -hsh -pvk "$EC_PVK" -pbk "$EC_PBK" -dat "$lic_pps" -smp -dbg
 } || {
   date -u
-  cat $(ls -lt ~/.ec/*.log | tail -1)
+  ls -al ~/.ec/
+  ls -lt ~/.ec/*.log | tail -1
 }
 
 : 'EC_LIC_PPS=$(agent -hsh -pvk $EC_PVK -pbk $EC_PBK -dat $lic_pps -smp)

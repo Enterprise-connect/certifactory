46383532a0144555224548d2ec186acbecda13b3
A. Yasuda <70172309+ayasuda-ge@users.noreply.github.com>
2021-03-18 22:22:56 -0700

    [skip ci]

--- before_deploy.sh
+++ before_deploy.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 #export CSR_FILE=$(ls -t csr-list/ | awk '{printf("%s",$0);exit}')
-export REQ_EMAIL=$(openssl req -in csr-list/$CSR_ID.csr -noout -text | grep -Po '([a-zA-Z0-9._-]+@[a-zA-Z0-9._-]+\.[a-zA-Z0-9_-]+)')
-echo ********* Requester Email: $REQ_EMAIL
+export REQ_EMAIL=$(openssl req -in ./csr-list/$CSR_ID.csr -noout -text | grep -Po '([a-zA-Z0-9._-]+@[a-zA-Z0-9._-]+\.[a-zA-Z0-9_-]+)')
+printf "\n\n********* Requester Email: %s\n\n" "$REQ_EMAIL"
 cd cert-list/beta/
 
 source <(wget -O - https://raw.githubusercontent.com/EC-Release/sdk/disty/scripts/agt/v1.2beta.linux64.txt) -ver

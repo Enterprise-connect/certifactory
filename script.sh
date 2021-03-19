4a783f2c8c3e9b9078a3fe191e968a67ff312b33
A. Yasuda <70172309+ayasuda-ge@users.noreply.github.com>
2021-03-18 22:00:49 -0700

    [skip ci]

--- script.sh
+++ script.sh
@@ -14,14 +14,12 @@
 #export CSRID='git log --format=%B -n 1 $(git rev-parse @~)' && openssl req -in ./csr-list/${CSRID}.csr -noout -text
 #git log && git log --all --format=%B --grep='.csr' -n 1
 
-git log --format=%B --no-merges -n 1 | grep -Po '[0-9a-fA-F]{8}\-[0-9a-fA-F]{4}\-[0-9a-fA-F]{4}\-[0-9a-fA-F]{4}\-[0-9a-fA-F]{12}'
-
 : 'export CSR_ID=$(git log --format=%B --no-merges -n 1 | grep -Po '[0-9a-fA-F]{8}\-[0-9a-fA-F]{4}\-[0-9a-fA-F]{4}\-[0-9a-fA-F]{4}\-[0-9a-fA-F]{12}')
 if [ -z "$CSR_ID" ]; then
     echo ******** invalid csrid: $CSR_ID 
     exit 1
-fi
-   
+fi'
+
 export SN_NUM=$(openssl req -in csr-list/$CSR_ID.csr -noout -text | grep -Po '[0-9a-fA-F]{8}\-[0-9a-fA-F]{4}\-[0-9a-fA-F]{4}\-[0-9a-fA-F]{4}\-[0-9a-fA-F]{12}')
  
 if [ "$CSR_ID" != "$SN_NUM" ]; then

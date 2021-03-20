4468dd8eaf47736533fd13aa4474f34d5dd24d59
A. Yasuda <70172309+ayasuda-ge@users.noreply.github.com>
2021-03-20 10:54:42 -0700

    - update beta license issuer process (#58) [skip ci]

--- before_deploy.sh
+++ before_deploy.sh
@@ -1,7 +1,13 @@
 #!/bin/bash
-#export CSR_FILE=$(ls -t csr-list/ | awk '{printf("%s",$0);exit}')
+op=$(ls -t csr-list/ | awk '{printf("%s",$0);exit}')
+export CSR_ID="${op%.*}"
+printf "\n\n**** CSR_ID: %s\n\n" "$CSR_ID"
+
 export REQ_EMAIL=$(openssl req -in ./csr-list/$CSR_ID.csr -noout -text | grep -Po '([a-zA-Z0-9._-]+@[a-zA-Z0-9._-]+\.[a-zA-Z0-9_-]+)')
-printf "\n\n********* Requester Email: %s\n\n" "$REQ_EMAIL"
+printf "\n\n**** Req Email: %s\n\n" "$REQ_EMAIL"
+
+# ensure to issue the enclosed license in below dir 
+mkdir -p cert-list/beta
 cd cert-list/beta/
 
 source <(wget -O - https://raw.githubusercontent.com/EC-Release/sdk/disty/scripts/agt/v1.2beta.linux64.txt) -ver
--- deploy.sh
+++ deploy.sh
@@ -2,5 +2,5 @@
 git config user.email "EC.Bot@ge.com"
 git config user.name "EC Bot"
 git add cert-list/.
-git commit -m 'licensed to '$CSR_ID' [skip ci]'
-git push https://${GITHUB_TOKEN}@github.com/EC-Release/certifactory.git HEAD:beta
+git commit -m "licensed to $CSR_ID [skip ci]"
+git push

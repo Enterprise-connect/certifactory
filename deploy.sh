cab9c1429e1f7cf744dae9da9fc602e1ec39f7f9
A. Yasuda <70172309+ayasuda-ge@users.noreply.github.com>
2021-03-21 23:03:13 -0700

    [skip ci]

--- deploy.sh
+++ deploy.sh
@@ -1,10 +1,10 @@
 #!/bin/bash
 git stash
 git checkout -b disty
-git pull
+git pull origin disty
 git stash apply
 git config user.email "EC.Bot@ge.com"
 git config user.name "EC Bot"
 git add cert-list/.
 git commit -m "licensed to $CSR_ID [skip ci]"
-git push -f
+git push

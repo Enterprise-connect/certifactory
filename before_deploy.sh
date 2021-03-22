e097f7abab89db9c341138d126948d36b75aa1a1
A. Yasuda <70172309+ayasuda-ge@users.noreply.github.com>
2021-03-21 20:53:14 -0700

    [skip ci]

--- before_deploy.sh
+++ before_deploy.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 #op=$(ls -t csr-list/ | awk '{printf("%s",$0);exit}')
 #op=$(git log -1 --pretty=%B csr-list)
-op=$(ls -1t csr-list/ | head -)
+op=$(ls -1t csr-list/ | head -1)
 export CSR_ID="${op%.*}"
 printf "\n\n**** CSR_ID: %s\n\n" "$CSR_ID"
 

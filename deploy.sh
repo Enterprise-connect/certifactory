c4380494127d556e725cb4ebc3c6c79cb8dc79d6
A. Yasuda <70172309+ayasuda-ge@users.noreply.github.com>
2021-03-24 20:48:38 -0700

    update disty push [skip ci]

--- deploy.sh
+++ deploy.sh
@@ -3,5 +3,7 @@ git config user.email "EC.Bot@ge.com"
 git config user.name "EC Bot"
 git add cert-list/.
 git commit -m "licensed to $CSR_ID [skip ci]"
+git fetch origin disty
 git checkout disty
-git push
+git merge beta
+git push origin disty

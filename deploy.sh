e7b0bbe1a261a76ddb95513c28e739cadc77094a
A. Yasuda <70172309+ayasuda-ge@users.noreply.github.com>
2021-07-15 22:45:22 -0700

    Update deploy.sh

--- deploy.sh
+++ deploy.sh
@@ -1,9 +1,11 @@
 #!/bin/bash
+cd ..
+git clone https://${EC_TKN}@github.com/EC-Release/x509.git
+cd x509
+mv ./../certifactory/cert-list/${DEV_ID}.cer ./crt-list/
+git add .
 git config user.email "EC.Bot@ge.com"
 git config user.name "EC Bot"
-git add cert-list/.
-git commit -m "licensed to $CSR_ID [skip ci]"
-git checkout -b disty
-git pull origin disty
-git merge beta
-git push origin disty
+git commit -m "crt ${DEV_ID} checked-in [skip ci]"
+git push
+cd ./../certifactory

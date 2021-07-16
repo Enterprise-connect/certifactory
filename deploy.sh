98e680dafb85875e4d115741d1d469676ea8174d
A. Yasuda <70172309+ayasuda-ge@users.noreply.github.com>
2021-07-15 22:57:25 -0700

    Update deploy.sh

--- deploy.sh
+++ deploy.sh
@@ -1,4 +1,14 @@
 #!/bin/bash
+echo "$DEV_ID" | tee -a  crt-list.txt > /dev/null
+git config user.email "EC.Bot@ge.com"
+git config user.name "EC Bot"
+git add crt-list.txt
+git commit -m "licensed to $CSR_ID [skip ci]"
+git checkout -b disty
+git pull origin disty
+git merge beta
+git push origin disty
+
 cd ..
 git clone https://${EC_TKN}@github.com/EC-Release/x509.git
 cd x509
